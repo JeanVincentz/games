441416f159c4cbcb65b0b5bed37b7c88995e5bf0
Jean Vincentz <55762464+JeanVincentz@users.noreply.github.com>
2020-06-26 11:45:25 +0200

    Boomclapcolors

---
+++ Projet04/assets/main.js
@@ -0,0 +1,17 @@
+const hexNum = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexBtn = document.querySelector('.hexBtn');
+const bodyBcg = document.querySelector('body');
+const hex = document.querySelector('.hex');
+
+hexBtn.addEventListener('click',boom);
+
+function boom (){
+    let hexCol = '#';
+
+    for(let i = 0; i<6; i++){
+        let random = Math.floor(Math.random()*hexNum.length);
+        hexCol += hexNum[random];
+    }
+    bodyBcg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+}
